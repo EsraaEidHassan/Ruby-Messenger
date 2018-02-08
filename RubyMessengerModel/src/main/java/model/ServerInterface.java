42e1b5bdb82d7031f46b5e04701c3a18c63f468a
USER <esraa.eidhassan@gmail.com>
2018-02-08 23:50:49 +0300

    server interface and implementation

---
+++ RubyMessengerModel/src/main/java/model/ServerInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author toshiba
+ */
+public interface ServerInterface extends Remote{
+    
+    //Esraa Hassan
+    public void register(ClientInterface client)throws RemoteException;
+    public void unregister(ClientInterface client)throws RemoteException;
+    public boolean signup_user(User user)throws RemoteException;
+    
+}
--- RubyMessengerServer/src/main/java/model/ServerImplementation.java
+++ RubyMessengerServer/src/main/java/model/ServerImplementation.java
@@ -5,17 +5,48 @@
  */
 package model;
 
+
+import controller.UserDao;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Vector;
 
 /**
  *
  * @author toshiba
  */
-public class ServerImplementation extends UnicastRemoteObject{
+public class ServerImplementation extends UnicastRemoteObject implements ServerInterface {
+    
+    //Esraa Hassan
+    private static Vector<ClientInterface> clients = new Vector<>();
     
     // Esraa Hassan
     public ServerImplementation() throws RemoteException {
+    }
+
+    @Override
+    public void register(ClientInterface client) throws RemoteException {
+        clients.add(client);
+    }
+    
+    @Override
+    public void unregister(ClientInterface client) throws RemoteException {
+        clients.remove(client);
+    }
+
+    //Esraa Hassan
+    @Override
+    public boolean signup_user(User user) throws RemoteException {
+        //this comment will be deleted if we resolve dependencies for com.oracle:ojdbc:jar:7
+        UserDao dao = new UserDao();
+        int result = dao.insertUser(user);//to be edited (add if condition to call register if succeeded)
+        if(result > 0){
+            return true;
+        }else{
+            return false;
+        }
+        //if signed up, server will call register and keep obj of client (Esraa) + redirect to home page with empty contact list
+        //if not . nothing will happen, redirect to signup page @ client
     }
     
 }
