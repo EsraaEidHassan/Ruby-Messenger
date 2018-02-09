af28e843aaf6221e029ee40c3b1b653e0a800d8c
Ahmed <ahmed@ahmed.com>
2018-02-09 20:02:54 +0200

    Sign up logic Complete

--- RubyMessengerClient/src/main/java/controller/SignupController.java
+++ RubyMessengerClient/src/main/java/controller/SignupController.java
@@ -7,7 +7,13 @@ package controller;
  */
 
 import java.net.URL;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,7 +21,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ToggleGroup;
+import model.Country;
+import model.ServerInterface;
+import model.User;
 
 /**
  * FXML Controller class
@@ -48,7 +57,12 @@ public class SignupController implements Initializable {
     //@FXML
     //AnchorPane mainAnchorPane;
     
+    // Abd Alfattah (Start)
+    ToggleGroup gender;
+    Registry registry;
+    ServerInterface server = null;
     
+    // Abd Alfattah (End)
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -61,6 +75,63 @@ public class SignupController implements Initializable {
         female.getStyleClass().add("button");
         signupButton.getStyleClass().add("button");
         
+        
+        // Abd Alfattah (Start)
+        
+        
+        try {
+            registry = LocateRegistry.getRegistry(2000);
+            server = (ServerInterface) registry.lookup("chat");
+        } catch (RemoteException | NotBoundException ex) {
+            Logger.getLogger(SignupController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // Abd Alfattah (End)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Abd Alfattah (End)
+        
     }    
     
+    
+    
+    // Abd Alfattah (Start)
+     
+    // Method to collect user info and send it to server
+    public boolean sendDataSignUp(){
+        String uName = username.getText();
+        String fName = fname.getText();
+        String lName = lname.getText();
+        String pass = password.getText();
+        String gend = gender.getSelectedToggle().getUserData().toString();
+        String em = email.getText();
+        
+        // dummy country object ( needs fix)
+        Country count = new Country();
+        boolean signUpStatus = false;
+        User userReg = new User(0, uName, pass, em, fName, lName, gend, count);
+        
+        try {
+            signUpStatus = server.signup_user(userReg);
+        } catch (RemoteException ex) {
+            Logger.getLogger(SignupController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if(signUpStatus == false){
+            System.out.println("Sign Up failed");
+            
+        }
+        return signUpStatus; 
+    }
+    
+    // Abd Alfattah (End)
 }
