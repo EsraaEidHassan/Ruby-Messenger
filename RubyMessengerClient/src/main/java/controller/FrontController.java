79799375427b7e09181af540857a72dd526f2def
Ahmed <ahmed@ahmed.com>
2018-02-09 12:14:27 +0200

    Updated FXML for Front Client Scene and Controller

--- RubyMessengerClient/src/main/java/controller/FrontController.java
+++ RubyMessengerClient/src/main/java/controller/FrontController.java
@@ -6,21 +6,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class FrontController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label noAccount;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    TextField username;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        noAccount.getStyleClass().add("label");
+        username.getStyleClass().add("username");
     }
     
     // abdelfata7
