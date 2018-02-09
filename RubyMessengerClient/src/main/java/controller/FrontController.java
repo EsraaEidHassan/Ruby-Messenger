5694e81fc667af97758c60382c7ed7dd8f2e08e3
immortal <khaledmamdouh273@gmail.com>
2018-02-09 15:58:51 +0200

    sign-in version 1

--- RubyMessengerClient/src/main/java/controller/FrontController.java
+++ RubyMessengerClient/src/main/java/controller/FrontController.java
@@ -12,8 +12,19 @@ import javafx.scene.layout.AnchorPane;
  // abdelfata7 end
 
 // khaled start
-    
-    
+
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader; 
+import model.ServerInterface;
+
 //khaled end
 public class FrontController implements Initializable {
     // abdelfata7 start
@@ -26,8 +37,9 @@ public class FrontController implements Initializable {
     // abdelfata7 end
     
     // khaled start
-    
-    
+    TextField userNameField; //temp
+    TextField passwordField; //temp
+    ServerInterface serverRef;
     //khaled end
     
     @Override
@@ -39,7 +51,14 @@ public class FrontController implements Initializable {
         // abdelfata7 end
         
         // khaled start
-    
+        
+        try {
+            Registry reg = LocateRegistry.getRegistry(2000);
+            serverRef = (ServerInterface) reg.lookup("chat");                        
+        } 
+        catch (RemoteException | NotBoundException ex) {
+            showServerError();
+        }
     
         //khaled end
     }
@@ -50,7 +69,32 @@ public class FrontController implements Initializable {
     // abdelfata7 end
     
     // khaled start
-    
-    
+    public void signInAction(){
+        String userName = userNameField.getText();
+        String password = passwordField.getText();
+        if(userName.trim().equals("") || password.trim().equals("") ){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("login error");
+            alert.setContentText("you must type your username and password to sign in");
+            alert.showAndWait();
+        }
+        else{
+            
+            if(true){ //suppose that signIn returned a user 
+                Stage mainStage =(Stage) userNameField.getScene().getWindow();
+                FXMLLoader loader = new FXMLLoader();
+                Parent root=null;
+                 //root = loader.load(getClass().getResource("UserMainScene.fxml").openStream());
+                Scene scene = new Scene(root);
+                mainStage.setScene(scene);
+            }
+        }
+    }
+    public static void showServerError(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("server error");
+        alert.setContentText("server not found! , try later");
+        alert.showAndWait();
+    }
     //khaled end
 }
